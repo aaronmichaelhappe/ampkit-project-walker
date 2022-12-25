4f748f9e182611589aa5072ef8dd38bea47af019
Aaron Happe <aaronmichaelhappe@gmail.com>
2022-12-24 19:48:44 -0800

    directory structure changes

--- src/test-content/src/utils/index.js
+++
@@ -1,4 +0,0 @@
-export function notNullOrNotUndefined(data) {
-  //TODO: check this is the function type I want to use
-  return data !== null && data !== undefined;
-}
--- src/test-content/src/views/DashboardView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="dashboard-view" :class="containerMainLayout">
-    <h1>Welcome to your damn dashboard.</h1>
-  </div>
-</template>
-<script setup>
-import { containerMainLayout } from "../../tailwind";
-</script>
